dfd60844077cda7bf5d9e9e2aa39e99cf1ca07e9
Roxana Mihaela Baba <alu0101339887@ull.edu.es>
2022-04-28 10:23:41 +0000

    Modificación

---
+++ src/EjercicioLAB/suma.ts
@@ -0,0 +1,113 @@
+// Programa en TypeScript que consista en una función que abre
+// el fichero con la lista de números, los sume, y escriba en
+// consola el resultado. Deberá invocar dicho programa desde
+// el manejador de la función watch expandiendo un subproceso.
+
+
+import {access, watch} from 'fs';
+import {spawn} from 'child_process';
+
+/**
+ * Función que abre el fichero con la lista de números,
+ * opera con ellos, y escribe en consola el resultado.
+ */
+export class Suma {
+  constructor(private file: string,
+              private operation: string,
+              private command: string,
+              private args: string[]) {
+    access(file, (err) => {
+      if (err) {
+        console.log(`File ${file} not found`);
+        return;
+      } else {
+        this.run();
+      }
+    });
+  }
+
+  /**
+   * Método que realiza la suma de los números de un fichero.
+   * @param file Nombre del fichero
+   */
+  private addFile(file: string) {
+    let sum = 0;
+    const lineReader = require('readline').createInterface({
+      input: require('fs').createReadStream(file),
+    });
+    lineReader.on('line', (line: number) => {
+      sum += +line;
+    });
+    lineReader.on('close', () => {
+      console.log(`${this.operation} of ${file} is ${sum}`);
+    });
+  }
+
+  /**
+   * Función que abre el fichero con la lista de números,
+   * opera con ellos, y escribe en consola el resultado.
+   */
+  private run() {
+    access(file, (err) => {
+      if (err) {
+        console.log(`File ${file} not found`);
+        return;
+      } else {
+        watch(this.file, (event, filename) => {
+          if (event === 'change') {
+            const child = spawn(this.command, [...this.args, this.file]);
+            let output = 0;
+            child.stdout.on('data', (data) => {
+              if (this.operation) {
+                if (this.operation === '+') {
+                  output += data;
+                } else if (this.operation === '-') {
+                  for (const num of file) {
+                    output -= Number(num);
+                  }
+                } else if (this.operation === '*') {
+                  for (const num of file) {
+                    output *= Number(num);
+                  }
+                } else if (this.operation === '/') {
+                  for (const num of file) {
+                    output /= Number(num);
+                  }
+                } else {
+                  console.log(`The operation is not supported`);
+                }
+                console.log(`Suma: ${output}`);
+              } else if (this.operation === '') {
+                this.addFile(this.file);
+              }
+            });
+            child.on('close', () => {
+              console.log(`Suma: ${output}`);
+            });
+          } else {
+            console.log(`${filename} has been deleted`);
+            throw new Error('${filename} has been deleted');
+          }
+        });
+      }
+    });
+  }
+}
+
+/**
+ * Línea de comandos para ejecutar el programa.
+ */
+const argv = process.argv;
+
+if (argv.length < 3) {
+  console.log('Usage: node suma.js numberList.txt [+, -, *, /...]');
+  process.exit(1);
+}
+const file = argv[2];
+if (argv.length === 3) {
+  new Suma(file, '', 'ls', ['-lh', file]);
+} else if (argv.length === 4) {
+  new Suma(file, argv[3], 'ls', []);
+} else {
+  new Suma(file, argv[3], argv[3], argv.slice(4));
+}
